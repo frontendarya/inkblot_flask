902ac5e64ba3c033d0c7625ebaf46cd1800fa0da
grigorevadd <d.grigoreva@i-teco.ru>
2024-06-14 09:35:32 +0300

    flask_structure - logic

--- neural/MeaningNeural.py
+++ neural/MeaningNeural.py
@@ -51,6 +51,39 @@ class Animal():
             return 1
 
 class Category():
+    from keras.preprocessing.text import Tokenizer
+    from keras.preprocessing.sequence import pad_sequences
+    from keras.models import Sequential
+    from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+    from sklearn.model_selection import train_test_split
+    from keras.utils.np_utils import to_categorical
+
+    # Подготовка данных
+    max_fatures = 2000
+    tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+    tokenizer.fit_on_texts(df['Text'].values)
+    X = tokenizer.texts_to_sequences(df['Text'].values)
+    X = pad_sequences(X)
+
+    # Создание модели
+    embed_dim = 128
+    lstm_out = 196
+
+    model = Sequential()
+    model.add(Embedding(max_fatures, embed_dim, input_length=X.shape[1]))
+    model.add(SpatialDropout1D(0.4))
+    model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Деление данных на обучающую и тестовую выборки
+    Y = pd.get_dummies(df['Category']).values
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+
+    # Обучение модели
+    batch_size = 32
+    model.fit(X_train, Y_train, epochs=7, batch_size=batch_size, verbose=2)
+
     (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=1000)
     # кодирование
     model = Sequential()
